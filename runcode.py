0aee4d0a8e420cc060734de515a7eaedda07b1ba
DXsmiley <dxsmiley@hotmail.com>
2015-06-27 14:23:15 +1000

    Environment variable to use shell

--- runcode.py
+++ runcode.py
@@ -10,11 +10,13 @@ class Crashed(Exception):
 	def __str__(self):
 		return 'Crash Report:\n' + self.data
 
-def execute(command = None, timeout = 1):
+def execute(command = None, timeout = 1, shell = None):
 	if command == None:
 		command = os.environ.get("CMD_INVOKE_CODE", "python {}").format("code.py")
+	if shell == None:
+		shell = int(os.environ.get("CMD_INVOKE_SHELL", 0)) == 1
 	try:
-		the_output = subprocess.check_output(command, timeout = timeout, stderr = subprocess.STDOUT, cwd = './rundir')
+		the_output = (command, timeout = timeout, stderr = subprocess.STDOUT, cwd = './rundir', shell = shell)
 		the_output = the_output.decode('utf-8').strip()
 		the_output = [i.strip() for i in the_output.split('\n')]
 		return the_output
